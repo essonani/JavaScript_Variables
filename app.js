59ac09eaadb2b0995254b62330250ac14d615011
essonani tchakpalla <essonani@hotmail.com>
2024-03-16 18:48:56 -0500

    var

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+const firstName="jhon";
+let lastName="doe";
+var age =21;
 
-// Exercise 1
+lastName="dolsoe";
+age=25;
 
-// Exercise 2
+let language="JavaScript";
+let createdYear="1995";
+let isCaseSensitive =true;
+console.log(`${language} was first release in ${createdYear}.`);
 
-// Exercise 3
+let product="T-shirt";
+let price =19.99;
+let isOnSale=false;
+let salePersentage=15;
+let stock=0;
+let inStock=false;
+let selectedSize="M";
+
+let title="Name of the wind";
+let author="Patrick Rothfuss";
+let pageCount= 722;
+let bookmark=456;
+let hasRead=true;
+
+console.log(`${product}  in size ${selectedSize} is $${price} with ${stock} in stock`);
